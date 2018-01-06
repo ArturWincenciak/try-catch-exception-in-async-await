f96549cbcd05aa080f652edd0adad0c224e626d3
ArturWincenciak <teo.vincent.com@gmail.com>
2018-01-06 15:53:13 +0100

    copy code form old version of post sorce code

--- TeoVincent.TryCatchExceptionInAsyncAwait/Program.cs
+++ TeoVincent.TryCatchExceptionInAsyncAwait/Program.cs
@@ -10,6 +10,55 @@ namespace TeoVincent.TryCatchExceptionInAsyncAwait
     {
         static void Main(string[] args)
         {
+            FuncCatchExAsync("1000");
+            WrongTryCatchException();
+            Console.ReadLine();
+        }
+
+        static Task<int> StrToIntAsync(string integer)
+        {
+            Func<object, int> strToInt = (object value) =>
+            {
+                return int.Parse((string)value);
+            };
+            return Task<int>.Factory.StartNew(strToInt, integer);
+        }
+
+        static async Task<int> ExceptionFuncAsync(string integer)
+        {
+            int result = await StrToIntAsync(integer);
+            throw new Exception("My exception.");
+            return result;
+        }
+
+        static async void FuncCatchExAsync(string integer)
+        {
+            try
+            {
+                int result = await ExceptionFuncAsync(integer);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        static Task GetTaskWithException()
+        {
+            Action action = () => { throw new Exception("My uncatched ex"); };
+            return Task.Factory.StartNew(action);
+        }
+
+        static async Task WrongTryCatchException()
+        {
+            try
+            {
+                await GetTaskWithException();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
     }
 }
