cd3889f9fcade75cdb7ba68149bd1a7849f4b428
ArturWincenciak <artur.wincenciak@gmail.com>
2018-01-06 15:48:56 +0100

    create new solution

---
+++ TeoVincent.TryCatchExceptionInAsyncAwait/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeoVincent.TryCatchExceptionInAsyncAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
